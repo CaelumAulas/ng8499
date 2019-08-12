cd2a3dad91fa85d05cde4dbfe52a7d3e1c502d4e
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-08-12 18:10:33 -0300

    Aula 01 - Inicio validacao template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng8499';
+
+  private _isEmailFormOpen = false;
+
+  listaEmails = [];
+
+  email = {
+    destinatario: '',
+    assunto: '',
+    conteudo: ''
+  }
+
+  get isEmailFormOpen() {
+    return this._isEmailFormOpen;
+  }
+
+  toggleEmailForm() {
+    this._isEmailFormOpen = !this.isEmailFormOpen;
+  }
+
+  enviarEmail(eventoSubmit: Event){
+    eventoSubmit.preventDefault();
+
+    const novoEmail = {
+      destinatario: this.email.destinatario,
+      assunto: this.email.assunto,
+      conteudo: this.email.conteudo
+    }
+
+    this.listaEmails.push(novoEmail)
+
+    this.email = {
+      destinatario: '',
+      assunto: '',
+      conteudo: ''
+
+    }
+  }
+
 }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cmail-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css', './header-search.css']
+})
+export class HeaderComponent {
+
+    isMenuOpen = false;
+
+    exibeMenu(){
+      this.isMenuOpen = !this.isMenuOpen;
+    }
+
+}
